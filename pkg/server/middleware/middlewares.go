076dabfa9cd00cf0d75ca02972eb2d1f5eb9689b
dan@fiamp.com <dan@fiamp.com>
2023-01-04 15:24:19 +0800

    retry中间件增加 listener, 修复 accesslog 中没有记录 retryAttempts 字段的问题

--- pkg/server/middleware/middlewares.go
+++ pkg/server/middleware/middlewares.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/containous/alice"
 	"github.com/traefik/traefik/v2/pkg/config/runtime"
+	"github.com/traefik/traefik/v2/pkg/middlewares/accesslog"
 	"github.com/traefik/traefik/v2/pkg/middlewares/addprefix"
 	"github.com/traefik/traefik/v2/pkg/middlewares/auth"
 	"github.com/traefik/traefik/v2/pkg/middlewares/buffering"
@@ -316,7 +317,9 @@ func (b *Builder) buildConstructor(ctx context.Context, middlewareName string) (
 		}
 		middleware = func(next http.Handler) (http.Handler, error) {
 			// TODO missing metrics / accessLog
-			return retry.New(ctx, next, *config.Retry, retry.Listeners{}, middlewareName)
+			//add save_retries listener to log retryAttempts
+			retryListener := accesslog.SaveRetries{}
+			return retry.New(ctx, next, *config.Retry, &retryListener, middlewareName)
 		}
 	}
 
